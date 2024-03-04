c086d7281315b1975f385829a2d6b6ecac4d7608
Ekzykes <atlaseuan@163.com>
2024-03-04 23:38:24 +0800

    添加查询排行榜 和获取用户名

--- src/libraries/diving_fish_request.py
+++ src/libraries/diving_fish_request.py
@@ -10,7 +10,27 @@ async def get_player_best_50(payload: Dict):
 async def get_player_plate_data(payload: Dict):
     async with aiohttp.request("POST", "https://www.diving-fish.com/api/maimaidxprober/query/plate", json=payload) as resp:
         result = await resp.json()
+        print(result)
         return result
 
+async def get_rating_ranking():
+    async with aiohttp.request("get", 'https://www.diving-fish.com/api/maimaidxprober/rating_ranking') as resp:
+        result = await resp.json()
+        return result
+    
+async def get_user_name_by_tentcent_user_id(payload: Dict):
+    async with aiohttp.request("POST", "https://www.diving-fish.com/api/maimaidxprober/query/player", json=payload) as resp:
+        if resp.status == 400:
+            return 400, ""
+        elif resp.status == 403:
+            return 403, ""
+        elif resp.status == 200:
+            result = await resp.json()
+            username = str(result['username'])
+            return 200, username
+        else:
+            return -1,""
 
-# asyncio.run(get_player_best_50({"username":"ryannn"}))
+
+
+# asyncio.run(get_player_best_50({"username":"ryan"}))
